6833c036dd0936aa0eedbe9838ec42001b55c362
Carlos Mañas <carlos@spacenomads.com>
2019-04-13 12:45:36 +0200

    Update JSON

--- terms.js
+++ terms.js
@@ -1,26 +1,28 @@
-{[
-  {
-    text: 'pizza',
-    total: 8
-  },
-  {
-    text: 'patata',
-    total: 2
-  },
-  {
-    text: 'pegatina',
-    total: 1
-  },
-  {
-    text: 'Móvil',
-    total: 7
-  },
-  {
-    text: 'Llaves',
-    total: 3
-  },
-  {
-    text: 'Cama',
-    total: 5
-  }
-]}
+{
+  "results": [
+    {
+      "text": "pizza",
+      "total": 8
+    },
+    {
+      "text": "patata",
+      "total": 2
+    },
+    {
+      "text": "pegatina",
+      "total": 1
+    },
+    {
+      "text": "Móvil",
+      "total": 7
+    },
+    {
+      "text": "Llaves",
+      "total": 3
+    },
+    {
+      "text": "Cama",
+      "total": 5
+    }
+  ]
+}
