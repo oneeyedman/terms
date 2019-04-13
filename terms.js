578b3e4c847255bbd84bbe6075598cbc3f23818d
Carlos Mañas <carlos@spacenomads.com>
2019-04-13 12:34:22 +0200

    Initial commit

---
+++ terms.js
@@ -0,0 +1,26 @@
+[
+  {
+    text: 'pizza',
+    total: 8
+  },
+  {
+    text: 'patata',
+    total: 2
+  },
+  {
+    text: 'pegatina',
+    total: 1
+  },
+  {
+    text: 'Móvil',
+    total: 7
+  },
+  {
+    text: 'Llaves',
+    total: 3
+  },
+  {
+    text: 'Cama',
+    total: 5
+  }
+]
